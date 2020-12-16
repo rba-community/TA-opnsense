a7fcaa9233f2ff06955b094682947ea1142f5de2
Zach Christensen <zachchristensen28@gmail.com>
2020-12-15 20:34:21 -0700

    added basic request call

--- bin/input_module_opnsense_system_information.py
+++ bin/input_module_opnsense_system_information.py
@@ -4,18 +4,8 @@ import os
 import sys
 import time
 import datetime
+import opnsense_constants as constant
 
-'''
-    IMPORTANT
-    Edit only the validate_input and collect_events functions.
-    Do not edit any other part in this file.
-    This file is generated only once when creating the modular input.
-'''
-'''
-# For advanced users, if you want to create single instance mod input, uncomment this method.
-def use_single_instance_mode():
-    return True
-'''
 
 def validate_input(helper, definition):
     # We have nothing to validate
@@ -24,17 +14,17 @@ def validate_input(helper, definition):
 def collect_events(helper, ew):
     # Get Credentials
     account = helper.get_arg('account')
-    API_KEY = account['username']
-    API_SECRET = account['password']
-    HOST = account['host']
-
+    api_key = account['username']
+    api_secret = account['password']
+    host = account['host']
+    
     # Get Log Level
     log_level = helper.get_log_level()
     helper.set_log_level(log_level)
-    helper.log_info('msg="Logging level set to: {}"'.format(str(log_level)))
+    helper.log_info(f'log_level="{log_level}"')
 
     # Get Stanza Name
-    STANZA_NAME = str(helper.get_input_stanza_names())
+    stanza_name = str(helper.get_input_stanza_names())
 
     # Get Proxy
     proxy = helper.get_proxy()
@@ -42,109 +32,31 @@ def collect_events(helper, ew):
     if proxy:
         if proxy['proxy_username']:
             helper.log_info('msg="Proxy is configured with authenticaiton"')
-            helper.log_debug('proxy_type="{}", proxy_url="{}", proxy_port="{}", proxy_username="{}"'.format(proxy['proxy_type'], proxy['proxy_url'], proxy['proxy_port'], proxy['proxy_username']))
-            proxy_string = '{}://{}:{}@{}:{}'.format(proxy['proxy_type'], proxy['proxy_username'], proxy['proxy_password'], proxy['proxy_url'], proxy['proxy_port'])
+            helper.log_debug(f'proxy_type="{proxy['proxy_type']}", proxy_url="{proxy['proxy_url']}", proxy_port="{proxy['proxy_port']}", proxy_username="{proxy['proxy_username']}"')
+            proxy_string = f'{proxy['proxy_type']}://{proxy['proxy_username']}:{proxy['proxy_password']}@{proxy['proxy_url']}:{proxy['proxy_port']}'
         else:
-            helper.log_info('msg="Proxy is configured"')
-            helper.log_debug('proxy_type="{}", proxy_url="{}", proxy_port="{}"'.format(proxy['proxy_type'], proxy['proxy_url'], proxy['proxy_port']))
-            proxy_string = '{}://{}:{}'.format(proxy['proxy_type'], proxy['proxy_url'], proxy['proxy_port'])
+            helper.log_info('msg="Proxy is configured with no authentication"')
+            helper.log_debug(f'proxy_type="{proxy['proxy_type']}", proxy_url="{proxy['proxy_url']}", proxy_port="{proxy['proxy_port']}"')
+            proxy_string = f'{proxy['proxy_type']}://{proxy['proxy_url']}:{proxy['proxy_port']}'
         
-        PROXY_CONFIG = {'http': proxy_string, 'https': proxy_string}
-
-
+        proxy_config = {'http': proxy_string, 'https': proxy_string}
+    else:
+        proxy_config = None
     
-    """Implement your data collection logic here
+    # URL
+    url = f'https://{host}/{constant.api_firmware_status}'
 
-    # The following examples get the arguments of this input.
-    # Note, for single instance mod input, args will be returned as a dict.
-    # For multi instance mod input, args will be returned as a single value.
-    opt_opnsense_account_credentials = helper.get_arg('opnsense_account_credentials')
-    # In single instance mode, to get arguments of a particular input, use
-    opt_opnsense_account_credentials = helper.get_arg('opnsense_account_credentials', stanza_name)
-
-    # get input type
-    helper.get_input_type()
-
-    # The following examples get input stanzas.
-    # get all detailed input stanzas
-    helper.get_input_stanza()
-    # get specific input stanza with stanza name
-    helper.get_input_stanza(stanza_name)
-    # get all stanza names
-    helper.get_input_stanza_names()
-
-    # The following examples get options from setup page configuration.
-    # get the loglevel from the setup page
-    loglevel = helper.get_log_level()
-    # get proxy setting configuration
-    proxy_settings = helper.get_proxy()
-    # get account credentials as dictionary
-    account = helper.get_user_credential_by_username("username")
-    account = helper.get_user_credential_by_id("account id")
-    # get global variable configuration
-    global_userdefined_global_var = helper.get_global_setting("userdefined_global_var")
-
-    # The following examples show usage of logging related helper functions.
-    # write to the log for this modular input using configured global log level or INFO as default
-    helper.log("log message")
-    # write to the log using specified log level
-    helper.log_debug("log message")
-    helper.log_info("log message")
-    helper.log_warning("log message")
-    helper.log_error("log message")
-    helper.log_critical("log message")
-    # set the log level for this modular input
-    # (log_level can be "debug", "info", "warning", "error" or "critical", case insensitive)
-    helper.set_log_level(log_level)
-
-    # The following examples send rest requests to some endpoint.
-    response = helper.send_http_request(url, method, parameters=None, payload=None,
-                                        headers=None, cookies=None, verify=True, cert=None,
-                                        timeout=None, use_proxy=True)
-    # get the response headers
-    r_headers = response.headers
-    # get the response body as text
-    r_text = response.text
-    # get response body as json. If the body text is not a json string, raise a ValueError
-    r_json = response.json()
-    # get response cookies
-    r_cookies = response.cookies
-    # get redirect history
-    historical_responses = response.history
-    # get response status code
-    r_status = response.status_code
-    # check the response status, if the status is not sucessful, raise requests.HTTPError
-    response.raise_for_status()
-
-    # The following examples show usage of check pointing related helper functions.
-    # save checkpoint
-    helper.save_check_point(key, state)
-    # delete checkpoint
-    helper.delete_check_point(key)
-    # get checkpoint
-    state = helper.get_check_point(key)
-
-    # To create a splunk event
-    helper.new_event(data, time=None, host=None, index=None, source=None, sourcetype=None, done=True, unbroken=True)
-    """
-
-    '''
-    # The following example writes a random number as an event. (Multi Instance Mode)
-    # Use this code template by default.
-    import random
-    data = str(random.randint(0,100))
-    event = helper.new_event(source=helper.get_input_type(), index=helper.get_output_index(), sourcetype=helper.get_sourcetype(), data=data)
-    ew.write_event(event)
-    '''
-
-    '''
-    # The following example writes a random number as an event for each input config. (Single Instance Mode)
-    # For advanced users, if you want to create single instance mod input, please use this code template.
-    # Also, you need to uncomment use_single_instance_mode() above.
-    import random
-    input_type = helper.get_input_type()
-    for stanza_name in helper.get_input_stanza_names():
-        data = str(random.randint(0,100))
-        event = helper.new_event(source=input_type, index=helper.get_output_index(stanza_name), sourcetype=helper.get_sourcetype(stanza_name), data=data)
+    try:
+        r = requests.get(url, proxies=proxy_config, auth=(api_key, api_secret), verify=False)
+    except Exception as e:
+        helper.log_error('msg="Unable to make api call"')
+        helper.log_debug(f'error_msg="{e}')
+    
+    if r.status_code == 200:
+        helper.log_info('msg="connection established", action="success"')
+        response = json.loads(r.text)
+        event = helper.new_event(source=helper.get_input_type(), index=helper.get_output_index(), sourcetype=helper.get_sourcetype(), data=json.dumps(response))
         ew.write_event(event)
-    '''
+    else:
+        helper.log_error('msg="unable to retrieve events", action="failed"')
+        helper.log_debug(f'status_code="{r.status_code}"')
---
+++ bin/opnsense_constants.py
@@ -0,0 +1,4 @@
+# API Endpoints
+
+# System Information
+api_firmware_status = 'api/core/firmware/status'
